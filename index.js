15518c1ce8a28d5a26507c19b2be370e05407ffb
PauloLima <159704780+PauloLimaj@users.noreply.github.com>
2024-09-10 07:39:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Author = require('./Author')
 
+// Criando o autor
 const john = new Author('John Wick')
 
+// Criando uma postagem
 const post = john.createPost('As crônicas de John Wick', 'bla bla bla bla')
 
+// Criando comentários
 post.addComments('Sopa', 'Eu amo as crônicas do cara aí')
 post.addComments('Alan', 'Eu odeio as crônicas desse cara muito feio!')
 
